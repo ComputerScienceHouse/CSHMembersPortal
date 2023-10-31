527d7220ed64ce5c0642753238b12d358046ee56
Joe Abbate <josephabbateny@gmail.com>
2023-10-31 13:10:36 -0400

    Please?

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,10 @@ sed -i 's/80/8080/g' /usr/local/apache2/conf/httpd.conf
 echo "LoadModule userdir_module modules/mod_userdir.so
 LoadModule rewrite_module modules/mod_rewrite.so
 LoadModule auth_openidc_module /usr/lib/apache2/modules/mod_auth_openidc.so
-<Directory /users>
-  #php_admin_value engine Off
-  Options all MultiViews -Indexes
-  DirectoryIndex index.html index.htm
-  <FilesMatch "\.php$">
-    Require all denied
-  </FilesMatch>
-  Require all granted
+<Directory /users/*/.html_pages/>
+        Options Indexes FollowSymLinks
+        AllowOverride None
+        Require all granted
 </Directory>
 <VirtualHost *:8080>
     UserDir .html_pages
